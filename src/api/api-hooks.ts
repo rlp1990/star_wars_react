9a139df94df0929c387ccc16375e071827f0307e
RafaelLopezPerez-cognizant <Rafael.LopezPerez@cognizant.com>
2022-04-30 16:46:10 +0200

    Added: api Connection class

---
+++ src/api/api-hooks.ts
@@ -0,0 +1,36 @@
+import { Params } from './types';
+
+export default class Connection {
+  private baseUrl = "https://swapi.dev/api/";
+  private doGetParams: string | undefined;
+
+  public setDoGetParams(params: Params): void {
+      let doGetParams: string = '?';
+    for (let key in params) {
+        doGetParams += `${key}=${params[key]}&`;
+    }
+    doGetParams += '&';
+    this.doGetParams = doGetParams.replace('&&', '');
+  }
+
+  public doGet(url: string, resolve: Function, reject: Function): void {
+    const promise = new Promise((resolve, reject) => {
+      fetch(this.baseUrl + url + this.doGetParams)
+        .then((res) => res.json())
+        .then(
+          (res) => {
+            resolve(res);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+    promise.then(response => {
+        resolve(response);
+    });
+    promise.catch(error => {
+        reject(error);
+    });
+  };
+}
